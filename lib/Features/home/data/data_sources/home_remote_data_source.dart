0fdf8e98cda61404a69066f180369e74c31bc247
waleed <inline.development.wa@gmial.com>
2025-04-12 08:01:22 +0200

    add home remote data source

---
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeatureBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
+
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  @override
+  Future<List<BookEntity>> fetchFeatureBooks() {
+    // TODO: implement fetchFeatureBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+  
+}
