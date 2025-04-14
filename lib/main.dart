8e0b4c16cfbcd2f14d97040fc77e4e2408d9b3ba
waleed <inline.development.wa@gmial.com>
2025-04-14 21:19:21 +0200

    create simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
-import 'package:bookly_app/Features/home/data/data_sources/home_local_data_source.dart';
-import 'package:bookly_app/Features/home/data/data_sources/home_remote_data_source.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
-import 'package:bookly_app/Features/home/domain/repos/home_repo.dart';
 import 'package:bookly_app/Features/home/domain/use%20cases/fetch_featured_books_use_case.dart';
 import 'package:bookly_app/Features/home/domain/use%20cases/fetch_newest_books_use_case.dart';
 import 'package:bookly_app/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/Features/home/presentation/manager/newets_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
-import 'package:dio/dio.dart';
+import 'package:bookly_app/core/utils/functions/setup_service_locator.dart';
+import 'package:bookly_app/core/utils/simple_bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
 
@@ -20,12 +17,13 @@ void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
 
+  setupServiceLocator();
+
   await Hive.openBox<BookEntity>(kFeaturedBox);
   await Hive.openBox<BookEntity>(kNewestBox);
+  Bloc.observer = SimpleBlocObserver();
   runApp(const BooklyApp());
 }
-
-final getIt = GetIt.instance;
 
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
@@ -37,14 +35,14 @@ class BooklyApp extends StatelessWidget {
         BlocProvider(create: (context) {
           return FeaturedBooksCubit(
             FetchFeaturedBooksUseCase(
-              homeRepo: HomeRepoImpel(
-                homeLocalDataSource: HomeLocalDataSourceImpl(),
-                homeRemoteDataSource: HomeRemoteDataSourceImpl(
-                  ApiService(
-                    Dio(),
-                  ),
-                ),
-              ),
+              homeRepo: getIt.get<HomeRepoImpel>(),
+            ),
+          );
+        }),
+        BlocProvider(create: (context) {
+          return NewestBooksCubit(
+            FetchNewestBooksUseCase(
+              homeRepo: getIt.get<HomeRepoImpel>(),
             ),
           );
         }),
